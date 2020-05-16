54d44696b80ee3d376568f2ead7dd7b6526a6145
kai815 <h.kaizuka815@gmail.com>
2020-05-16 12:07:01 +0900

    Expressのルーティング

--- lecture24/app.js
+++ lecture24/app.js
@@ -5,5 +5,14 @@ app.use(require("./lib/logger.js"));
 
 app.get("/", (req, res)=>{
   res.status(200).send("Hello Express");
+});
+app.get("/home/index", (req, res)=>{
+  res.status(200).send("OK");
 })
+
+app.get("/test/:id", (req, res)=>{
+  res.status(200).send(`test/${req.params.id}/のリクエストがきました`);
+})
+
+app.use("/user", require("./router/user.js"));
 app.listen(3000);
