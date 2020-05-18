b54166f74f92bd5f2edcb0bd72e518a7c819fbb9
kai815 <h.kaizuka815@gmail.com>
2020-05-18 18:36:12 +0900

    ファイルパスが間違っていたので修正

--- weblog/config/mongodb.config.js
+++ weblog/config/mongodb.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CONNECTION_URL: "mongodb://user:user@localhost:27017/wevlog",
+  CONNECTION_URL: "mongodb://user:user@localhost:27017/weblog",
   DATABSE: "weblog",
   OPTIONS: {
     family: 4
