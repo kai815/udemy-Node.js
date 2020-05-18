10c30416f34d627e66a5823ffaba3d5acedfcf45
kai815 <h.kaizuka815@gmail.com>
2020-05-18 15:25:11 +0900

    システムログ出力機能の実装

--- weblog/app.js
+++ weblog/app.js
@@ -1,3 +1,4 @@
+var systemlogger = require("./lib/log/systemlogger.js");
 var express = require("express");
 var app = express();
 
@@ -7,5 +8,7 @@ app.disable("x-powered-by");
 app.use("/public", express.static(__dirname +"/public/"+ (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use("/", require("./routes/index.js"));
+
+app.use(systemlogger());
 
 app.listen(3000);
