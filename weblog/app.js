e138e0a1c9ccde3081e53c7c71c5549b5785828c
kai815 <h.kaizuka815@gmail.com>
2020-05-18 16:31:44 +0900

    アプリケーションログの実装

--- weblog/app.js
+++ weblog/app.js
@@ -11,4 +11,12 @@ app.use("/", require("./routes/index.js"));
 
 app.use(systemlogger());
 
+//---
+var logger = require("./lib/logger/logger").application;
+// 下記の2行を何度も書くのは面倒なのでlogger.jsで使いまわしやすく
+// logger.addContext("key", "test");
+// logger.error("message");
+logger.error("test","message2");
+//---
+
 app.listen(3000);
