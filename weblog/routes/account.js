b2288efcbf8e6389eb0d049b6a5f9dec98660eba
kai815 <h.kaizuka815@gmail.com>
2020-05-19 14:50:49 +0900

    記事登録の確認画面

--- weblog/routes/account.js
+++ weblog/routes/account.js
@@ -13,6 +13,27 @@ var createRegistData = function(body) {
   };
 };
 
+var validateRegistData = function(body) {
+  var isValidated = true, errors = {};
+
+  if(!body.url){
+    isValidated = false;
+    errors.url = "URLが未入力です。'/'から始まるURLを入力して下さい。";
+  }
+
+  if(body.url && /^\//.test(body.url) === false) {
+    isValidated = false;
+    errors.url = "'/'から始まるURLを入力して下さい。";
+  }
+
+  if(!body.title) {
+    isValidated = false;
+    errors.titel = "タイトルが未入力です。任意のタイトルを入力してください。";
+  }
+
+  return isValidated ? undefined :errors;
+};
+
 router.get("/", (req, res) => {
   res.render("./account/index.ejs");
 });
@@ -24,6 +45,16 @@ router.get("/posts/regist", (req, res) => {
 router.post("/posts/regist/input", (req, res) => {
   var original = createRegistData(req.body);
   res.render("./account/posts/regist-form.ejs", {original});
+});
+
+router.post("/posts/regist/confirm", (req, res) => {
+  var original = createRegistData(req.body);
+  var errors = validateRegistData(req.body);
+  if (errors) {
+    res.render("./account/posts/regist-form.ejs", {errors, original});
+    return;
+  }
+  res.render("./account/posts/regist-confirm.ejs", { original });
 });
 
 module.exports = router;
